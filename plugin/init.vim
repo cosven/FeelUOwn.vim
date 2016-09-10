5d69c2fd8e1051826a68d9a56a45c37458966ff8
cosven <yinshaowen241@gmail.com>
2016-09-11 02:06:49 +0800

    add toggle user window
    1. make user window not editable
    2. can toggle user window
    
    TODO:
    1. goto corrent window after close user window
    2. improve mpg123 intergration
    3. ...

--- plugin/init.vim
+++ plugin/init.vim
@@ -6,15 +6,47 @@
 "
 let s:songsBufferName = "songs.feeluown" 
 
+" --------------------------------------
+" code in this block are copied from  http://majutsushi.github.com/tagbar/
+"
+" goto win
+function! s:goto_win(winnr, ...) abort
+    let cmd = type(a:winnr) == type(0) ? a:winnr . 'wincmd w'
+                                     \ : 'wincmd ' . a:winnr
+    let noauto = a:0 > 0 ? a:1 : 0
+
+    if noauto
+        noautocmd execute cmd
+    else
+        execute cmd
+    endif
+endfunction
+
+" s:mark_window() {{{2
+" Mark window with a window-local variable so we can jump back to it even if
+" the window numbers have changed.
+function! s:mark_window() abort
+    let w:tagbar_mark = 1
+endfunction
+
+" s:goto_markedwin() {{{2
+" Go to a previously marked window and delete the mark.
+function! s:goto_markedwin(...) abort
+    let noauto = a:0 > 0 ? a:1 : 0
+    for window in range(1, winnr('$'))
+        call s:goto_win(window, noauto)
+        if exists('w:tagbar_mark')
+            unlet w:tagbar_mark
+            break
+        endif
+    endfor
+endfunction
+
+" --------------------------------------
+
 
 filetype plugin on
 au! BufRead,BufNewFile *.feeluown set filetype=feeluown
-
-func! InitVimOptions(...)
-    echo "feeluown: init vim options"
-    setlocal noswapfile
-    normal! gg
-endfunc
 
 func! SongsBufferExists()
     if bufwinnr(s:songsBufferName) > 0
@@ -30,5 +62,23 @@ func! CreateSongsBuffer(...)
     endif
 endfunc
 
-autocmd Filetype feeluown call InitVimOptions()
-" call CreateSongsBuffer()
+func! ToggleFeeluownUser(...)
+    let fuwinnr = bufwinnr('user.feeluown')
+    if fuwinnr != -1
+        " user window exist
+        if winnr() == fuwinnr
+            bwipeout
+            " Try to jump to the correct window after closing
+        else
+            call s:mark_window()
+            call s:goto_win(fuwinnr)
+            bwipeout
+            call s:goto_markedwin()
+        endif
+    else
+        FeeluownShowUser
+    endif
+endfunc
+
+
+nnoremap <leader>ftu :call ToggleFeeluownUser()<cr>
--- rplugin/python3/views.py
+++ rplugin/python3/views.py
@@ -4,7 +4,6 @@ import sys
 import neovim
 
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
-print(os.path.abspath(os.path.dirname(__file__)))
 
 from fuo.model import NUserModel
 
@@ -14,47 +13,49 @@ class Feeluown(object):
     def __init__(self, vim):
         self.vim = vim
         self.user = NUserModel.load()
+        self.playlists = self.user.playlists
         self.songs = []
-        self.playlists = []
 
     @neovim.command('Feeluown')
     def feeluown(self):
-        self.playlists = self.user.playlists
         self.show_user()
 
-    def load_songs(self, pid):
-        pass
-
+    @neovim.command('FeeluownShowUser')
     def show_user(self):
         self.show_user_playlists()
 
     def show_user_playlists(self):
-        self.vim.command('echo "feeluown loaded."')
-        self.vim.command('split playlists.feeluown')
-        self.vim.command('buffer playlists.feeluown')
-        self.vim.command('set buftype=nofile')
-        self.vim.command('set bufhidden=hide')
-        self.vim.command('set nonumber')
+        self.vim.command('vsplit user.feeluown')
+        self.vim.command('buffer user.feeluown')
         self.vim.command('nnoremap <buffer> <CR> :FeeluownLoadPlaylist<CR>')
         self.vim.current.buffer[:] = [
             str(index) + '. ' + p.name
             for index, p in enumerate(self.playlists)]
+        self.vim.command('vertical resize 40')
+        self.vim.command('setlocal nonumber nomodifiable buftype=nofile '
+                         'bufhidden=wipe readonly nobuflisted noswapfile '
+                         'winfixwidth')
 
     def show_playlist_songs(self, playlist):
         self.songs = playlist.songs
-        self.vim.command('echo "feeluown loaded."')
         self.vim.command('vsplit songs.feeluown')
         self.vim.command('buffer songs.feeluown')
-        self.vim.command('set buftype=nofile')
-        self.vim.command('set bufhidden=hide')
-        self.vim.command('set nonumber')
         self.vim.command('nnoremap <buffer> <CR> :FeeluownPlay<CR>')
-        self.vim.current.buffer[:] = [
-            str(index) + '. ' + song.title + ' - ' + song.artists_name
+        self.vim.current.buffer[0] = ' | 歌曲名 | 歌手名 | 专辑名 '
+        self.vim.current.buffer[1:] = [
+            str(index) + ' | ' + song.title + ' | ' + song.artists_name +
+            ' | ' + song.album_name
             for index, song in enumerate(playlist.songs)]
+        self.vim.command('Tab /|')
+        self.vim.command('setlocal nonumber nomodifiable buftype=nofile '
+                         'bufhidden=wipe readonly nobuflisted noswapfile')
 
     @neovim.command('FeeluownLoadPlaylist')
     def load_playlist(self):
+        if self.vim.current.buffer.name.split('/')[-1] != 'user.feeluown':
+            self.vim.command('echo "current buffer is not user.feeluown"')
+            return 0
+
         self.vim.command('echo "feeluown load playlist"')
         index = int(self.vim.current.line.split('.')[0])
         playlist = self.playlists[index]
@@ -62,10 +63,13 @@ class Feeluown(object):
 
     @neovim.command('FeeluownPlay')
     def play(self):
+        if self.vim.current.buffer.name.split('/')[-1] != 'songs.feeluown':
+            self.vim.command('echo "current buffer is not songs.feeluown"')
+            return 0
+
         self.vim.command('echo "feeluown play song"')
-        index = int(self.vim.current.line.split('.')[0])
+        index = int(self.vim.current.line.split('|')[0])
         song = self.songs[index]
         self.vim.command('echo "url is %s"' % song.url)
-        # os.system('mplayer %s' % song.url)
         subprocess.Popen(['mpg123', song.url])
         self.vim.command('echo "playing?"')
